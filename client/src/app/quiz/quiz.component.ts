3b7735210ba3fae627c61240ec6273020a752a40
fahuamancaja <47445881+fahuamancaja@users.noreply.github.com>
2020-10-27 20:13:54 -0400

    Updated Changes

---
+++ client/src/app/quiz/quiz.component.ts
@@ -0,0 +1,30 @@
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { take } from 'rxjs/operators';
+import { User } from 'src/app/_models/user';
+import { AccountService } from 'src/app/_services/account.service';
+
+@Component({
+  selector: 'app-quiz',
+  templateUrl: './quiz.component.html',
+  styleUrls: ['./quiz.component.css']
+})
+export class QuizComponent implements OnInit {
+  @ViewChild('editForm') editForm: NgForm;
+  user: User;
+  @HostListener('window:beforeunload', ['$event']) unloadNotification($event: any) {
+    if(this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
+
+  constructor(private accountService: AccountService, 
+    private toastr: ToastrService) {
+      this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
+   }
+
+  ngOnInit(): void {
+  }
+
+}
